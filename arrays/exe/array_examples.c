4d0daaaae3b9486c15406f37c4c1ecb6f57f6f8a
Adam The Canadian <adam.jarrett.clare@gmail.com>
2022-01-05 16:43:57 -0700

    Add checking sum of two elements

--- arrays/exe/array_examples.c
+++ arrays/exe/array_examples.c
@@ -5,14 +5,13 @@
 void Exercise_3_1() {
   static const int num_students = 20;
   static const int num_classes = 5;
-  /* Sample problem taken from "Data Structures in C - Chapter 3 #1 */
   /* Given 20 students, and the marks for the students 5 classes, find different statistics */
   /* We are not actually going to do a 2D array [][], but a 1D array to make life simpler */
   int marks[num_students * num_classes];
   /* First initialize the marks to random numbers between 0 and 100 */
   for (int i = 0; i < 20; i++) {
     for (int j = 0; j < 5; j++) {
-      /* Make the  */
+      /* Make the numbers between 0 and 100 */
       marks[i * num_classes + j] = rand() % 100;
     }
   }
@@ -30,7 +29,29 @@ void Exercise_3_1() {
 
 }
 
+void Exercise_3_2() {
+  /* Given an array of 100 integers, find pairs that sum to 50 */
+  static const int n = 101;
+  int arr[n];
+  for (int i = 0; i < n; i++) {
+    /* Keep elements between 0 and 50 */
+    arr[i] = rand() % 50;
+    printf("arr[%d]: %d\n", i, arr[i]);
+  }
 
+
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = i + 1; j < n; j++) {
+      int sum = arr[i] + arr[j];
+      if (sum == 50) {
+        printf("%d and %d sum to 50\n", arr[i], arr[j]);
+      }
+    }
+  }
+
+}
 int main() {
+  /* Sample problems taken from "Data Structures in C - Chapter 3*/
   Exercise_3_1();
+  Exercise_3_2();
 }
